b5ff844cd2c43fc01e2b2bdeb743af9366666ce7
董旭腾 <260739028@qq.com>
2018-04-09 22:47:33 +0800

    practice

---
+++ 课程练习/LS11/js/demo07_02.js
@@ -0,0 +1,22 @@
+/**
+ * Created by dongxuteng on 2018/4/9.
+ */
+
+
+(function () {  // IIFE开始
+    var x = 20;
+ })();           // IIFE结束
+ 
+ // function nameSpace() {
+ //     var x = 20;
+ // }
+ // nameSpace();
+ 
+ 
+ 
+ /*
+ //由于ES5没有块作用域，即使写成如下形式依然有问题
+ if(true){
+     var x = 30;
+ }
+ */
---
+++ 课程练习/LS11/js/demo10.js
@@ -0,0 +1,24 @@
+/**
+ * Created by dongxuteng on 2018/4/9.
+ */
+
+
+
+//变量共享问题
+for (var i = 0; i < 5; i++) {
+    setTimeout(function() {
+        console.log(new Date, i);
+    }, 1000*i);
+}
+//console.log("i：",i);
+*/
+
+
+//通过IIFE解决变量共享问题
+for (var i = 0; i < 5; i++) {
+    (function(j) {  // j = i
+        setTimeout(function() {
+            console.log(new Date, j);
+        }, 1000*i);
+    })(i);
+}
