613ae3a0d87d4b57a1c90497cda54ae0dfd08a75
董旭腾 <260739028@qq.com>
2018-04-15 22:14:56 +0800

    practice

---
+++ 课程练习/LS12/js/demo13.js
@@ -0,0 +1,18 @@
+/**
+ * Created by dongxuteng on 2018/4/15.
+ */
+
+function counter() {
+    var n = 0;
+    return {
+        count:function () {return ++n;},
+        reset:function () {n = 0;return n;}
+    }
+}
+var c = counter();
+var d = counter();
+console.log(c.count());
+console.log(d.count());
+console.log(c.reset());
+console.log(c.count());
+console.log(d.count());
