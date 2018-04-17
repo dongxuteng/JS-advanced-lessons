5bf69f8e676c281ebaf054c8af25050ad634c498
董旭腾 <260739028@qq.com>
2018-04-17 15:59:48 +0800

    parctice

---
+++ 课程练习/LS13/js/demo01.js
@@ -0,0 +1,14 @@
+/**
+ * Created by dongxuteng on 2018/4/16.
+ */
+
+var obj = {
+    num:10,
+    str:"Hi",
+    show:function(){
+        console.log(this.str);
+    }
+};
+console.log(obj.num);//10
+console.log(obj.str);//Hi
+obj.show();			 //Hi
---
+++ 课程练习/LS13/js/demo02.js
@@ -0,0 +1,34 @@
+/**
+ * Created by dongxuteng on 2018/4/16.
+ */
+
+
+console.log(typeof Array);
+console.log(typeof Function);
+console.log(typeof Date);
+console.log(typeof Number);
+console.log(typeof String);
+console.log(typeof Boolean);
+console.log(typeof Math);//和上边一样么？为什么
+console.log(typeof JSON);
+
+
+
+console.log(Object instanceof Function);
+console.log(Object instanceof Object);
+console.log(Boolean instanceof Function);
+console.log(Boolean instanceof Object);
+console.log(String instanceof Function);
+console.log(String instanceof Object);
+console.log(Number instanceof Function);
+console.log(Number instanceof Object);
+console.log(Function instanceof Function);
+console.log(Function instanceof Object);
+console.log(Array instanceof Function);
+console.log(Array instanceof Object);
+console.log(Date instanceof Function);
+console.log(Date instanceof Object);
+console.log(Math instanceof Function);
+console.log(Math instanceof Object);
+console.log(JSON instanceof Function);
+console.log(JSON instanceof Object);
