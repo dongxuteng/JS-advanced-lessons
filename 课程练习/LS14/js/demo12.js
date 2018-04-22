7d59cf59f884def56991648571bfe8ac175777f7
董旭腾 <260739028@qq.com>
2018-04-22 19:58:28 +0800

    practice

---
+++ 课程练习/LS14/js/demo12.js
@@ -0,0 +1,19 @@
+/**
+ * Created by dongxuteng on 2018/4/22.
+ */
+
+ //属性特性描述符
+var obj = {x:5};
+Object.defineProperty(obj,"y",{
+    configurable:false,
+    writable:false,
+    enumerable:true,
+    value:6
+});
+Object.defineProperty(obj,"z",{
+    configurable:false,
+    value:7
+});
+Object.getOwnPropertyDescriptor(obj,"x");
+//Object.getOwnPropertyDescriptor(obj,"y");
+//Object.getOwnPropertyDescriptor(obj,"z");
